63155e7922068b494fbc306b89d2534791a7d554
robertnunn <robnunn10@gmail.com>
2020-10-17 12:23:27 -0400

    minor fix for blank lines

--- bot.py
+++ bot.py
@@ -274,6 +274,7 @@ async def magic20(ctx, cmd='', arg=''):
     """
     with open('magic20.txt') as f:
         lines = f.read().split('\n')
+    lines.remove('')
 
     if cmd.lower() == 'add':
         ok_to_add = True
