169e743e1e9e4d6e60985338963aff30ed3f0dad
robertnunn <robnunn10@gmail.com>
2020-10-18 11:38:21 -0400

    added separate input sanitization function

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ drop_dice_re = re.compile(r'^(\d+)?[L|H]$')
 num_re = re.compile(r'^\d*$')
 
 # magic20 rules
-char_limit = 120
+magic20_char_limit = 120
 
 
 @bot.event
@@ -268,6 +268,16 @@ async def dnd(ctx, adventure=0):
     await ctx.send(msg)
 
 
+def sanitize_input(arg: str):
+    # sanitizin' muh inputs
+    arg = arg.replace('\n', '')
+    exceptions = '!#$%&? ^*()-_=+[]{};:\'",./<>`~'
+    for i in arg:
+        if not i.isalnum() and i not in exceptions:
+            arg = arg.replace(i, '')
+    return arg
+
+
 @bot.command()
 async def magic20(ctx, cmd='', arg=''):
     """
@@ -282,23 +292,21 @@ async def magic20(ctx, cmd='', arg=''):
 
     if cmd.lower() == 'add':
         ok_to_add = True
-        arg = arg.replace('\n', '')
-        exceptions = '!#$%&? ^*()-_=+[]{};:\'",./<>`~'
-        for i in arg:  #sanitizin' muh inputs
-            if not i.isalnum() and i not in exceptions:
-                arg = arg.replace(i, '')
-
-        if len(arg) > char_limit: # enforce character limit
+        arg = sanitize_input(arg)
+        msg = list()
+        if len(arg) > magic20_char_limit: # enforce character limit
             ok_to_add = False
-            msg = f'Error: message is over the character limit ({char_limit}) by {len(arg)-char_limit} characters'
+            msg.append(f'Error: message is over the character limit ({magic20_char_limit}) by {len(arg)-magic20_char_limit} characters')
         
-        if arg not in lines:  # no dupes
+        if arg in lines:  # no dupes
             ok_to_add = False
+            msg.append(f'Error: message `{arg}` is already in the list')
         
-        if ok_to_add:
+        if ok_to_add:  # this must come last
             with open('magic20.txt', 'a') as f:
                 f.write(f'\n{arg}')
             msg = f'Added\n{arg}\nto the list of responses as number {len(lines)+1}'
+            
     elif cmd.lower() == 'del':
         try:  # basic error checking
             arg = int(arg)
