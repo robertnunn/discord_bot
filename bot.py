2a5e7c5044d683cecc4f7cbf33ebb66a7f8ea064
robertnunn <robnunn10@gmail.com>
2022-02-28 20:43:15 -0500

    command shuffle on putin counter

--- bot.py
+++ bot.py
@@ -47,8 +47,8 @@ async def putin_l_counter(ctx, cmd='', new_l=''):
     Keep track of the many, many Ls that putin has taken since invading Ukraine!
     cmd is 'add': adds the following QUOTED string to the list
     cmd is 'all': shows all the Ls
-    cmd is 'random': picks a random L
-    Just "!putin": show the latest L on the list
+    cmd is 'latest': show the latest L on the list
+    Just "!putin": picks a random L
     """
     putin_file = 'putin.txt'
     cmd = cmd.lower()
@@ -59,16 +59,16 @@ async def putin_l_counter(ctx, cmd='', new_l=''):
     elif new_l == '' and cmd != 'add':
         with open(putin_file) as p:
             l_list = p.read().splitlines()
-        if cmd == 'random':
-            the_l = random.choice(l_list)
-            msg = f"Putin has taken at least {len(l_list)} Ls since invading Ukraine. Here's a random putin L: {the_l}"
+        if cmd == 'latest':
+            the_l = l_list[-1]
+            msg = f"Putin has taken at least {len(l_list)} Ls since invading Ukraine. Here's the latest putin L: {the_l}"
         elif cmd == 'all':
             all_ls = '\n'.join(l_list)
             all_ls = f'```\n{all_ls}```'
             msg = f"Putin has taken at least {len(l_list)} Ls since invading Ukraine. Here's all of them. {all_ls}"
         else:
-            the_l = l_list[-1]
-            msg = f"Putin has taken at least {len(l_list)} Ls since invading Ukraine. Here's the latest putin L: {the_l}"
+            the_l = random.choice(l_list)
+            msg = f"Putin has taken at least {len(l_list)} Ls since invading Ukraine. Here's a random putin L: {the_l}"
 
     await ctx.send(msg)
 
